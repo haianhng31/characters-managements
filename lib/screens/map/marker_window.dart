5f75317722f4849e9e746809fdde54663839b3a2
Hennessy Nguyen <anguyen137@fordham.edu>
2024-05-23 16:28:57 +0700

    change the required fields

--- lib/screens/map/marker_window.dart
+++ lib/screens/map/marker_window.dart
@@ -1,22 +1,19 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:clippy_flutter/triangle.dart';
-import 'package:flutter_rpg/models/character.dart';
-// import 'package:flutter_rpg/services/character_store.dart';
-import 'package:flutter_rpg/services/firestore_service.dart';
 import 'package:flutter_rpg/shared/styled_text.dart';
-// import 'package:provider/provider.dart';
 
 class MarkerWindow extends StatefulWidget {
   const MarkerWindow({
     super.key,
-    required this.characterId,
+    required this.characterName,
+    required this.characterImg,
     required this.date,
     required this.characterIdsAssociated,
     required this.description,
   });
 
-  final String characterId;
+  final String characterName;
+  final String characterImg;
   final String date;
   final List<String> characterIdsAssociated;
   final String description;
@@ -26,41 +23,17 @@ class MarkerWindow extends StatefulWidget {
 }
 
 class _MarkerWindowState extends State<MarkerWindow> {
-  late final String characterName;
-  late final String characterImg;
   late final List<String> charactersAssociated;
-  bool isLoading = true;
-
-  void fetchCharacters() async {
-    DocumentSnapshot<Character> snapshot = await FirestoreService.getCharacter(widget.characterId);
-    Character character = snapshot.data()!;
-
-    List<String> images = await Future.wait(widget.characterIdsAssociated.map((id) async {
-      DocumentSnapshot<Character> snapshot = await FirestoreService.getCharacter(id);
-      Character characterAssociated = snapshot.data()!;
-      return 'assets/img/vocations/${characterAssociated.vocation.image}';
-    }).toList());
-
-    print("Character is fetched from Firestore: ${character.name}");
-
-    setState(() {
-      charactersAssociated = images;
-      characterName = character.name;
-      characterImg = 'assets/img/vocations/${character.vocation.image}';
-      isLoading = false; 
-    });
-  }
 
   @override
   void initState() {
-    fetchCharacters();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return  Column(
-      children: isLoading ? [const Center(child: CircularProgressIndicator())] : 
+      children: 
       [
         Expanded(
           child: Container(
@@ -79,7 +52,7 @@ class _MarkerWindowState extends State<MarkerWindow> {
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: [
                       ClipOval( child: Image.asset(
-                        characterImg,
+                        widget.characterImg,
                         fit: BoxFit.cover,
                         width: 30, 
                         height: 30
@@ -90,7 +63,7 @@ class _MarkerWindowState extends State<MarkerWindow> {
                         mainAxisAlignment: MainAxisAlignment.start,
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          StyledHeading(characterName),
+                          StyledHeading(widget.characterName),
                           StyledText(widget.date)
                         ],
                       )
@@ -108,20 +81,20 @@ class _MarkerWindowState extends State<MarkerWindow> {
                     ),
                   ),
                   const SizedBox(height: 1.0),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: 
-                      charactersAssociated.map((img) => Padding(
-                        padding: const EdgeInsets.all(1.0),
-                        child: ClipOval( child: Image.asset(
-                          img,
-                          fit: BoxFit.cover,
-                          width: 18, 
-                          height: 18
-                          )
-                        ),
-                      )).toList()
-                  ),
+                  // Row(
+                  //   mainAxisAlignment: MainAxisAlignment.end,
+                  //   children: 
+                  //     charactersAssociated.map((img) => Padding(
+                  //       padding: const EdgeInsets.all(1.0),
+                  //       child: ClipOval( child: Image.asset(
+                  //         img,
+                  //         fit: BoxFit.cover,
+                  //         width: 18, 
+                  //         height: 18
+                  //         )
+                  //       ),
+                  //     )).toList()
+                  // ),
                 ],
               ),
             ),
